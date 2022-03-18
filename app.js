17aa428a101a70451ce00b33d518ada6e7311abb
JacquelineMuralles <jacquelinenmuralles@gmail.com>
2022-03-18 17:03:41 -0500

    Added random number functionality

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ GAME FUNCTION:
   //Game values
   let  min = 1,
        max = 10,
-       winningNum = 2,
+       winningNum = getRandomNum(min, max);
        guessesLeft = 3; 
 
   // UI Elements
@@ -24,6 +24,13 @@ GAME FUNCTION:
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max; 
+
+//Play again event listener
+game.addEventListener('mousedown', function(e) {
+  if(e.target.className === 'play-again'){
+    window.location.reload();
+  }
+})
 
 // Listen for guess
 guessBtn.addEventListener('click', function(){
@@ -69,7 +76,14 @@ function gameOver(won, msg) {
   message.style.color = color;
   //set message
   setMessage(msg);
-  
+  // Play Again?
+  guessBtn.value = 'Play Again?';
+  guessBtn.className += 'play-again';
+}
+
+//Get winning number
+function getRandomNum(min, max){
+  return Math.floor(Math.random()*(max - min + 1) + min);
 }
 
 // Set Message
